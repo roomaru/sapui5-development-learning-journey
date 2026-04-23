26a60a9ab0d2e71dfe0da0ce328ccdf834a4608d
Dayeon Ryu <dadajco@mail.ulsan.ac.kr>
2026-04-23 08:47:25 +0000

    Unit 7: Component

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,10 +5,14 @@ sap.ui.define([
         "use strict";
 
         return UIComponent.extend("sap.training.exc.Component", {
+            metadata: {
+                manifest: "json"
+            },
 
-
-
-
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+            }
         });
     }
 );
