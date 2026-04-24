5854f621dc65c251007e5de129772f2d04cce264
Dayeon Ryu <dadajco@mail.ulsan.ac.kr>
2026-04-24 07:51:26 +0000

    Unit 9: Fragment

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, syncStyleClass) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
+            onSave: function () {
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "sap.training.exc.view.Dialog"
+                    }).then(function (oDialog) {
+                        syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
+                        return oDialog;
+                    }.bind(this));
+                }
+                this.pDialog.then(function (oDialog) {
+                    oDialog.open();
+                });
+            },
 
-
+            onCloseDialog: function () {
+                this.byId("dialog").close();
+            }
         });
     });
